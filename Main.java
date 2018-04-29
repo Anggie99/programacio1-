5d7dc59558b9aa7168a7eede9fa0e944b713c17c
anggie moyota <stephaniemoyota@gmail.com>
2018-04-28 20:23:09 -0500

    se añadio un nuevo version

---
+++ Main.java
@@ -0,0 +1,24 @@
+package Paquete;
+
+class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x[]={8,01,99};
+		/*x = new int[3];
+		x[0]=12;
+		x[1]=06;
+		x[2]=14;
+		
+		System.out.println(x[0]);*/
+		for (int i=0; i<x.length; i++)
+		{
+			System.out.println("El valor del arreglos son:"+i+"el valor es:"+x[i]);
+		}
+
+	}
+
+}
